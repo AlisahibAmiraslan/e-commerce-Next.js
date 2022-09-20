b9bf452f382d14f6a74b7e08240d0f042ee8abe3
Alisahib Amiraslan <=>
2022-09-21 00:28:40 +0300

    fdfd

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { BannerTop, TopTen, BannerMiddle } from "../components";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>E-commerce</title>
+      </Head>
       <BannerTop />
       <TopTen />
       <BannerMiddle />
