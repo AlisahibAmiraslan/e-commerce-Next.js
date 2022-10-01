6e6ae6344dd92951e92f378ffc4ffcb63d8f23df
Alisahib Amiraslan <=>
2022-10-01 23:53:24 +0300

    cxcx

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,8 @@ import { Header, Footer } from "./../components";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { StoreProvider } from "../context/ProductContext";
-import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [msg, setMsg] = useState([]);
   return (
     <>
       <StoreProvider>
