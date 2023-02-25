76b745a613f2334bce60f18ede7d82b240db5c09
Alisahib Amiraslan <=>
2023-02-26 02:58:34 +0300

    sad

--- components/Partial/Header.js
+++ components/Partial/Header.js
@@ -5,13 +5,9 @@ import Logo from "./../../public/Images/logo.png";
 import Image from "next/image";
 import { ProductContext, Store } from "../../context/ProductContext";
 import { BsSearch } from "react-icons/bs";
-import { auth } from "../../Config/Config";
-import { signOut } from "firebase/auth";
-import { onAuthStateChanged } from "firebase/auth";
 
 function Header() {
   const router = useRouter();
-  const [user, setUser] = useState("");
   const [openMenu, setOpenMenu] = useState(false);
   const [IsOpen, setIsOpen] = useState(false);
   const { state, dispatch } = useContext(Store);
@@ -76,32 +72,6 @@ function Header() {
     }
   }, [router]);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const uid = user.uid;
-        setUser(uid);
-      } else {
-        // User is signed out
-        // ...
-        console.log("user is logged out");
-      }
-    });
-  }, []);
-
-  const handleLogout = () => {
-    signOut(auth)
-      .then(() => {
-        router.push("/login");
-        setTimeout(() => {
-          router.reload(window.location.pathname);
-        }, 200);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   return (
     <>
       <nav className="w-full bg-white z-[100] h-auto">
@@ -159,48 +129,6 @@ function Header() {
                 </button>
               </form>
             </div>
-
-            {!user && (
-              <div className="mr-2">
-                <Link href="/login">
-                  <a>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                      />
-                    </svg>
-                  </a>
-                </Link>
-              </div>
-            )}
-            {user && (
-              <div className="mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6 cursor-pointer"
-                  onClick={handleLogout}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-                  />
-                </svg>
-              </div>
-            )}
 
             {/* shopping cart bag */}
             <div className="shopping-cart relative">
