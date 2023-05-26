9610bd9d62316ad567b18ab80228e8b24c0f42d0
Ali <100287885+AlisahibAmiraslan@users.noreply.github.com>
2023-05-26 11:56:39 +0300

    update: image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-	images: {
-		domains: ["api.lorem.space", "placeimg.com"],
-	},
+  images: {
+    domains: [
+      "api.lorem.space",
+      "placeimg.com",
+      "picsum.photos",
+      "cdna.artstation.com",
+      "e0.pxfuel.com",
+      "api.escuelajs.co",
+    ],
+  },
 };
